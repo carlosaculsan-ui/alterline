772880758b2e323e4893f07a5d50823e7373491a
Carlo <carlosaculsan@gmail.com>
2026-05-20 23:16:36 +0800

    merchant dashboard, order management, weight confirmation, earnings chart

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import AdminUsers from './pages/admin/AdminUsers'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
 import SearchPage from './pages/SearchPage'
+import MerchantDashboard from './pages/MerchantDashboard'
 
 function KeyedEntryPage() {
   const { id } = useParams()
@@ -57,6 +58,7 @@ export default function App() {
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/admin/users" element={<AdminUsers />} />
         </Route>
+        <Route path="/merchant" element={<MerchantDashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AuthProvider>
